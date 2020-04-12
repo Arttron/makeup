5fefe7bd6f59fc82a976dd5c827303f99af1e28d
Artem Romanov <artemr.maxs@gmail.com>
2020-04-12 18:24:40 +0300

    make public

---
+++ public/component---src-pages-shop-js-2a2d308f770b23ac49bc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{vg2q:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the shop page"),o.a.createElement("p",null,"Welcome to shop"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-shop-js-2a2d308f770b23ac49bc.js.map
